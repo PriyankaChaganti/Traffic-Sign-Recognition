da99003b176f4922ab4a43cfb252e9acbe191338
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-01-14 11:45:27 +0530

    cv2 imshow demo

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -2,8 +2,19 @@ import cv2
 import getpass
 import platform
 import datetime
+import glob
+import numpy as np
 
-print(getpass.getuser())
-print(platform.uname())
-print(datetime.datetime.now())
-print(cv2.__version__)
+#print(getpass.getuser())
+#print(platform.uname())
+#print(datetime.datetime.now())
+#print(cv2.__version__)
+path="C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/*.*"
+for file in glob.glob(path):
+ print(file)
+ a = cv2.imread(file)
+ print(a)
+c = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+cv2.imshow('Color image', c)
+k = cv2.waitKey(10000)
+cv2.destroyAllWindows()
