8885d13f7a08378e96cae131d58562ae5ad6a6b7
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-01-21 09:15:54 +0530

    Update main.py

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -11,10 +11,9 @@ import numpy as np
 #print(cv2.__version__)
 path="C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/*.*"
 for file in glob.glob(path):
- print(file)
  a = cv2.imread(file)
- print(a)
  c = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
  cv2.imshow('Color image', c)
- k = cv2.waitKey(10000)
+ cv2.waitKey(10000)
+ cv2.destroyAllWindows()
 
