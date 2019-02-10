71d92a04a932069d59aec619bee44f0f052f1b99
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-10 21:14:38 +0530

    task 10

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -1,4 +1,11 @@
+from read_images import read_image_annotations
 
+folderpath=""
+training_data=""
+read_image_annotations(folderpath,training_data)
+"""
+
+read_image_annotations()
 training_data=make_dataset(folder_path,folders_list)
 
 test_data=make_dataset(folder_path,folders_list)
@@ -7,4 +14,4 @@ multi_class_classifier=multi_class_classifier(training_data,svm_params)
 
 results=test_classifier(multi_class_classifier,test_data)
 
-
+"""
--- robust_svm/read_images.py
+++ robust_svm/read_images.py
@@ -1,12 +1,43 @@
 import csv
+import os
 
-def make_dataset(dat_set_path,data_set_list)
+def make_dataset(dat_set_path,data_set_list):
     """
     :param dat_set_path: path to datasets(training/test)
     :param data_set_list: folders list
     :return: location
     """
 def read_image_annotations(data_path,data_set_name):
+    dataSetList = ['00013', '00014', '00015', '00017', '00019']
+
+    dataset = '00000'
+
+    currentLoc = os.getcwd()+'/'
+    dataloc = 'C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/Images/'
+    #finalData = Data.Data()
+
+    for dataset in dataSetList:
+
+        fileList = os.listdir(dataloc+dataset+'/')
+
+        imageFiles = []
+        fileLoc = dataloc+dataset+'/'
+        annFile = "GT-"+dataset+".csv"
+
+        annData = []
+        with open(fileLoc+annFile, 'r') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+
+            count = 0
+            for row in spamreader:
+                if(count > 0):
+                    annData.append(row)
+                count = count + 1
+        print(annData)
+
+
+
+
 
 
     """
@@ -15,7 +46,10 @@ def read_image_annotations(data_path,data_set_name):
     :param data_set_name: 
     :return: 
     """
-def get_cropped_image(file_path,annotation)
+def get_cropped_image(file_path,annotation):
+
+
+
     """
     
     :param file_path: 
