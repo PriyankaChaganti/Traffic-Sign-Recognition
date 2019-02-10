fa11b97e8fc404269060feb5ab2f109e7b1fdb9b
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-10 11:16:00 +0530

    changes

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -1,14 +1,6 @@
 import cv2
-import getpass
-import platform
-import datetime
 import glob
-import numpy as np
 
-#print(getpass.getuser())
-#print(platform.uname())
-#print(datetime.datetime.now())
-#print(cv2.__version__)
 path="C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/Images/*/*.*"
 for file in glob.glob(path):
  a = cv2.imread(file)
