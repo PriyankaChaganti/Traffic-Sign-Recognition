9f6b3e4447ed632dc29c30c47b075e152e703008
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-11 17:35:53 +0530

    task 17/18
    no data in hog_Feature folders...hence code imagining data present in folders

--- robust_svm/process_images.py
+++ robust_svm/process_images.py
@@ -1,3 +1,6 @@
+from os.path import join
+import numpy as np
+import glob
 def get_successors(pixd,xlim,ylim):
     """
     
@@ -39,3 +42,32 @@ def image_to_feature_vector(image):
     :param image: 
     :return: 
     """
+def get_hog_features(data_set_name,image_file_name):
+    """
+
+    :param data_set_name:
+    :param image_file_name:
+    :return:
+    """
+    hog_feature_path = join(data_set_name,image_file_name)
+    files = sorted(glob.glob(hog_feature_path + '/*.npy'))
+    arrays = []
+    for f in files:
+        arrays.append(np.load(f))
+    hog_feature_data = np.concatenate(arrays)
+    return hog_feature_data
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Test the function read_image_annotations
+    data_set_name='../data/training_data/Features_HOG/'
+    image_file_name = 'HOG_3'
+    hog_feature_data= get_hog_features(data_set_name,image_file_name)
+    print(hog_feature_data)
