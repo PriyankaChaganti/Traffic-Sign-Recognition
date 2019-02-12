7a05d1271e2280c6a763d9fab0cd95ed042e0551
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-12 21:53:34 +0530

    Update data_set.py

--- robust_svm/data_set.py
+++ robust_svm/data_set.py
@@ -1,29 +1,33 @@
 from enum import Enum
 from random import shuffle
-class ImageDataset:
+class ImageDataset():
     def add_image(self,data_sets_path,data_set,annotation):
         """
 
         :param annotation:
         :return:
         """
-    def add_row(self,class_id,feature_vector):
-        dict = {}
-        xVal = []
-        dict['x'] = class_id
-        dict['y'] = feature_vector
-        self.data.append(dict)
 
+
+    def add_row(self,class_id,feature_vector):
         """
 
         :param class_id:
         :param feature_vector:
         :return:
         """
+
+        dict = {}
+        dict['class_id'] = class_id
+        dict['feature_vector'] = feature_vector
+        self.data.append(dict)
+
+
     def __init__(self,data,class_ids,hog):
         self.data=[]
         self.class_ids=[]
-        hog = False
+        self.hog=False
+
 
     def shuffle_indexes(self):
         newData = []
@@ -45,3 +49,8 @@ class AnnotationMapping(Enum):
     ROIy2 = 6
     Classid = 7
 
+if __name__ == '__main__':
+    im = ImageDataset(data=[100,200], class_ids=[1,2], hog=False)
+    print(im.data)
+    print(im.class_ids)
+    print(im.hog)
