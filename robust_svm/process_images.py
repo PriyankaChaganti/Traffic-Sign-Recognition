cbd1739b33ec45fb84c54613ca2d9831c16b3808
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-14 15:19:31 +0530

    update

--- robust_svm/process_images.py
+++ robust_svm/process_images.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import cv2
 from settings import hog3_feature_path
+import settings
 def get_successors(pixd,xlim,ylim):
     """
     
@@ -55,21 +56,11 @@ def get_hog_features(data_class_id,image_file_name):
     :return:
     """
     hog3_feature_path = "C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/Features_HOG/HOG_3"
-    var2 = isdir(hog3_feature_path)
     image_feature_filename = image_file_name.replace(".ppm",".txt")
     hog_feature_path = join(hog3_feature_path,data_class_id,image_feature_filename)
-    #The error is with for loop....isfile(hog_feature_path) returns true
-    print(hog_feature_path)
-    print(isfile(hog_feature_path))
-    var1=open(hog_feature_path)
-    print(var1)
-    """
-    arrays = []
-    for image_feature_filename in hog_feature_path:
-        arrays.append(np.load(image_feature_filename))
-    hog_feature_data = np.concatenate(arrays)
-    return hog_feature_data
-"""
+    with open(hog_feature_path, 'r') as input_file:
+        lines_of_file = [line for line in input_file]
+    return np.array(lines_of_file)
 
 
 
--- robust_svm/settings.py
+++ robust_svm/settings.py
@@ -1,6 +1,7 @@
-from os.path import join,isfile,exists
-import inspect
-hog3_feature_path = "C:/Users/ch.srivamsi priyanka/Documents/GitHub/traffic_sign_svm/data/training_data/Features_HOG/HOG_3"
-#hog3_feature_path = os.path.hog3_feature_path(os.path.abspath(inspect.stack()[0][1]))
-var1 = isfile(hog3_feature_path)
-print(var1)
+from os.path import join,isfile,exists,abspath
+project_path = abspath(__file__+"/../../")
+training_data = "data/training_data"
+hog3_feature_path = "Features_HOG/HOG_3"
+images_path = "Images"
+training_data_folder = join(project_path,training_data)
+hog3_folder = join(project_path,training_data,hog3_feature_path)
