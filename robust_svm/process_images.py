e61fef9834a7ff63cdd76dfd8351791d7459a6be
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-17 22:41:05 +0530

    Update process_images.py

--- robust_svm/process_images.py
+++ robust_svm/process_images.py
@@ -42,6 +42,34 @@ def highlight_invariant_threashold(image):
     :param image: 
     :return: 
     """
+    image = get_cropped_image(file_path,annotation)
+    hsvimage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+    #blue
+    treshLow1 = np.array([235, 40, 0], dtype=np.uint8)
+    treshHigh1 = np.array([255, 255, 255], dtype=np.uint8)
+    img1 = cv2.inRange(hsvimage, treshLow1, treshHigh1)
+    #high colour
+    treshLow1 = np.array([0, 40, 0], dtype=np.uint8)
+    treshHigh1 = np.array([255, 255, 255], dtype=np.uint8)
+    img2 = cv2.inRange(hsvimage, treshLow1, treshHigh1)
+    #non-black
+    treshLow1 = np.array([0, 0, 30], dtype=np.uint8)
+    treshHigh1 = np.array([255, 255, 230], dtype=np.uint8)
+    img3 = cv2.inRange(hsvimage, treshLow1, treshHigh1)
+    #red
+    treshLow2 = np.array([0, 0, 0])
+    treshHigh2 = np.array([10, 255, 255])
+    img4 = cv2.inRange(hsvimage, treshLow2, treshHigh2)
+
+    cv2.bitwise_or(img1,img4,img1)
+    cv2.bitwise_and(img1,img2,img1)
+    cv2.bitwise_and(img1,img3,img1)
+
+    kernel = np.ones((2,2),np.uint8)
+    closing = cv2.morphologyEx(img1,cv2.MORPH_CLOSE,kernel, iterations = 2)
+    img1 = cv2.morphologyEx(closing,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+
 
 def image_to_feature_vector(image):
     """
