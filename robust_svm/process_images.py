77ea2170f6035362513f6ed9c357dc6e71457606
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-19 11:16:53 +0530

    update docstrings for task 39

--- robust_svm/process_images.py
+++ robust_svm/process_images.py
@@ -1,71 +1,71 @@
-from os.path import join,isfile,exists,isdir
 import numpy as np
-import glob
-import cv2
-from settings import hog3_folder
+
+from robust_svm.settings import *
+
 
 def get_successors(pixd,xlim,ylim):
     """
-    
-    :param pixd: 
-    :param xlim: 
-    :param ylim: 
-    :return: 
+
+    :param pixd:
+    :param xlim:
+    :param ylim:
+    :return:
     """
 
 
 
 def grow_region(image):
     """
-    
-    :param image: 
-    :return: 
+
+    :param image:
+    :return:
     """
 def hole_fill(image):
     """
-    
-    :param image: 
-    :return: 
+
+    :param image:
+    :return:
     """
 def get_seeds(pixel,xlim,ylim,image):
     """
-    
-    :param pixel: 
-    :param xlim: 
-    :param ylim: 
-    :param image: 
-    :return: 
+
+    :param pixel:
+    :param xlim:
+    :param ylim:
+    :param image:
+    :return:
     """
 def highlight_invariant_threashold(image):
     """
-    
-    :param image: 
-    :return: 
+
+    :param image:
+    :return:
     """
 
 def image_to_feature_vector(image):
     """
-    
-    :param image: 
-    :return: 
-    """
-def get_hog_features(data_class_id,image_file_name):
+
+    :param image:
+    :return:
     """
 
+
+def read_feature_file(features_folder_path, data_class_id, image_file_name):
+    """
+    The function reads image file and returns its corresponding HOG feature file in the form of a numpy array
     :param data_class_id:The name of directory holding image_feature_filename. (Example = '00000')
     :param image_file_name:The name of image file.(Example = '00000_00001.ppm')
     :return:hog_data_array
     """
-
-    image_feature_filename = image_file_name.replace(".ppm",".txt")
-    hog_feature_path = join(hog3_folder,data_class_id,image_feature_filename)
-    hog_data_array = np.fromfile(hog_feature_path,dtype=float,count=-1,sep=" ")
+    feature_filename = image_file_name.replace(".ppm",".txt")
+    feature_file_path = join(features_folder_path, data_class_id, feature_filename)
+    hog_data_array = np.fromfile(feature_file_path)
     return hog_data_array
 
 
 if __name__ == "__main__":
-    # Test the function read_image_annotations
-    data_class_id='00000'
+    # Test the function read_feature_file
+    data_class_id = '00000'
     image_file_name = '00000_00000.ppm'
-    hog_feature_data= get_hog_features(data_class_id,image_file_name)
-    print(hog_feature_data)
+    features_folder_path = join(training_data_folder, hog3_path)
+    hog_feature_data = read_feature_file(features_folder_path, data_class_id, image_file_name)
--- robust_svm/read_images.py
+++ robust_svm/read_images.py
@@ -1,44 +1,46 @@
 import csv
 import os
-from os.path import join
-from settings import *
-from data_set import *
 
-def make_dataset(data_set_path,data_set_list,hog):
+from robust_svm.data_set import *
+
+
+def make_dataset(data_path, feature_path, data_set_list):
     """
-    The function iterates on the list of datasets and adds the image from the annotation file to the ImageDataset instance via add_image()
-    :param data_set_path: path to datasets(training/test)
+    The function iterates on the list of datasets and adds the image from the annotation
+    file to the ImageDataset instance via add_image()
+    :param data_path: path to datasets(training/test)
+    :param feature_path: path to features folder relative to data_path
     :param data_set_list: folders list
     :return: ImageDataset instance
     """
+    images_folder_path = join(data_path, images_path)
+    features_folder_path = join(data_path, feature_path)
+    im = ImageDataset(images_path=images_folder_path, features_path=features_folder_path)
 
-    im = ImageDataset()
     #Iterates on the datasets in the dataset list
     for dataset in data_set_list:
         print("Processing dataset {}".format(dataset))
+        folder_path = join(images_folder_path, dataset)
         #Read the annotation file in the dataset
-        annotated_data = read_image_annotations(data_set_path ,dataset)
+        annotated_data = read_image_annotations(folder_path, dataset)
         #Iterates on all annotations in the annotation file
         for eachann in annotated_data:
-            folder_path = join(data_set_path, dataset)
             folder_files = os.listdir(folder_path)
             if(eachann[AM.Filename] in folder_files):
-                im.add_image(data_set_path,dataset,eachann)
+                im.add_image(dataset, eachann)
         print("Done processing images in the dataset {}".format(dataset))
     return im
 
 
-def read_image_annotations(datasets_path, dataset_name):
+def read_image_annotations(folder_path, dataset_name):
     """
     The function reads the annotation file using csv.reader and returns the annotation data as python list
     :param datasets_path: The path where images and annotation file are present. Example: data/training_data/00001
     :param dataset_name: The directory name. Example:00001
     :return: annotated_data
     """
-
-    annotation_file_folder_path = join(datasets_path, dataset_name)
     annotation_file_name = "GT-{}.csv".format(dataset_name)
-    annotation_file_path = join(annotation_file_folder_path , annotation_file_name)
+    annotation_file_path = join(folder_path, annotation_file_name)
     annotated_data = []
     with open(annotation_file_path, 'r') as csvfile:
         csv_file_reader = csv.reader(csvfile, delimiter=';')
@@ -47,9 +49,9 @@ def read_image_annotations(datasets_path, dataset_name):
             annotated_data.append(row)
     return annotated_data
 
+
 if __name__ == "__main__":
     # Test the function read_image_annotations
-
-    datasets_path = images_folder
-    data_set_list = ['00013','00015']
-    make_dataset(datasets_path,data_set_list,False)
+    sample_feature_path = join(training_data_folder, hog3_path)
+    sample_data_set_list = ['00013', '00015']
+    test_datset = make_dataset(training_data_folder, sample_feature_path, sample_data_set_list)
