36a8652eb91339ce2cb2ff779f761464e1efd6ff
PriyankaChaganti <chpriyanka1729@gmail.com>
2019-02-24 02:07:02 +0530

    Update main.py

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -5,7 +5,7 @@ from robust_svm.classifier import MultiClassClassifier
 
 data_path = training_data_folder
 feature_path = hog3_path
-data_set_list = ['00013', '00015']
+data_set_list = ['trial_dataset']
 training_data = make_dataset(data_path, feature_path, data_set_list)
 epoch = 1
 kernel_type = 'linear'
@@ -13,6 +13,7 @@ svmclassifier = MultiClassClassifier(training_data,epoch,{"r0":1,"c":1,kernel_ty
 
 svmclassifier_pickle = pickle.dump(svmclassifier,open("../data/dumps/svmclassifier.p","wb"))
 svmclassifier_unpickle = pickle.load(open("../data/dumps/svmclassifier.p","rb"))
+test_data = make_dataset(test_data_folder,hog3_path,data_set_list)
 
 training_data_pickle = pickle.dump(training_data, open( "../data/dumps/training_data.p", "wb" ))
 training_data_unpickle = pickle.load( open( "../data/dumps/training_data.p", "rb" ) )
