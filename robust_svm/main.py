eef503eee7294f0538728402182dd9abbd7e55d4
vineela <vinnygkakarla@gmail.com>
2019-03-07 01:39:02 +0530

    latest commit
    on all data sets accuracy

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -1,6 +1,7 @@
 from os.path import join
 import pickle
 from time import time
+from os import listdir
 
 from robust_svm.classifier import MultiClassClassifier
 from robust_svm.read_images import make_dataset
@@ -11,11 +12,13 @@ from robust_svm.svm_utils import test_classifier
 # Data Processing
 ########################################################################################
 # Compile a list of datasets that would be used for training and testing the classifier
-data_set_list = ['00013', '00014','00015','00017','00019']
+data_sets = join(settings.training_data_folder,settings.images_path)
+data_set_list = listdir(data_sets)
+#data_set_list = ['00013', '00014','00015','00017','00019']
 
 # Set the type of image features that would be used for training ex: hog_1, hog_3,
 # custom_features etc.
-feature_path = settings.hog3_path
+feature_path = settings.hog1_path
 
 # Set the path where the training data and test data would be stored as pickled objects
 training_data_pickle_path = join(settings.dumps_folder, "training_data.p")
@@ -41,8 +44,8 @@ test_data = pickle.load(open(test_data_pickle_path, "rb"))
 # Set the parameters for Multi-class Classifier
 epochs = 100
 svm_params = {
-    'r0': 0.9,
-    'C': 1,
+    'r0': 0.5,
+    'C': 100,
     'kernel_type': 'linear'
 }
 multi_svm_classifier_pickle_path = join(settings.dumps_folder, 'multi_svm_classifier.p')
@@ -53,7 +56,7 @@ print('Building the classifier using epochs={0} and svm_params={1}'.format(
 #multi_svm_classifier = MultiClassClassifier(training_data, epochs, svm_params)
 #pickle.dump(multi_svm_classifier, open(multi_svm_classifier_pickle_path, "wb"))
 # Comment the above two lines and uncomment the following line to avoid re-building classifier
-#multi_svm_classifier = pickle.load(open(multi_svm_classifier_pickle_path, "rb"))
+multi_svm_classifier = pickle.load(open(multi_svm_classifier_pickle_path, "rb"))
 
 
 ##########################################################################################
@@ -61,6 +64,10 @@ print('Building the classifier using epochs={0} and svm_params={1}'.format(
 ###########################################################################################
 accuracy_results = test_classifier(multi_svm_classifier,test_data)
 print("Accuracy Results:")
+sum = 0
 for class_id, results in accuracy_results.items():
     results['accuracy_percentage'] = format((results['right'] / results['total'])*100,'.2f')
+    sum = sum + float(format((results['right'] / results['total'])*100,'.2f'))
     print('class id: {}'.format(class_id), results)
+avg = sum/42
+print(avg)
