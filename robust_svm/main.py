07af19ff16b76a932056ff5d665dbdebab8bbaae
vineela <vinnygkakarla@gmail.com>
2019-03-10 23:18:17 +0530

    Update main.py
    commit to

--- robust_svm/main.py
+++ robust_svm/main.py
@@ -13,12 +13,12 @@ from robust_svm.svm_utils import test_classifier
 ########################################################################################
 # Compile a list of datasets that would be used for training and testing the classifier
 data_sets = join(settings.training_data_folder,settings.images_path)
-data_set_list = listdir(data_sets)
-#data_set_list = ['00013', '00014','00015','00017','00019']
+#data_set_list = listdir(data_sets)
+data_set_list = ['00010','00012','00013', '00014','00015','00017','00033','00034','00035','00036','00038']
 
 # Set the type of image features that would be used for training ex: hog_1, hog_3,
 # custom_features etc.
-feature_path = settings.hog1_path
+feature_path = settings.hog3_path
 
 # Set the path where the training data and test data would be stored as pickled objects
 training_data_pickle_path = join(settings.dumps_folder, "training_data.p")
@@ -26,17 +26,17 @@ test_data_pickle_path = join(settings.dumps_folder, "test_data.p")
 
 # Process the training images and load the training data
 print('Loading training data')
-#training_data = make_dataset(settings.training_data_folder, feature_path, data_set_list)
-#pickle.dump(training_data, open(training_data_pickle_path, "wb"))
+training_data = make_dataset(settings.training_data_folder, feature_path, data_set_list)
+pickle.dump(training_data, open(training_data_pickle_path, "wb"))
 # Comment the above two lines and uncomment the following line to avoid re-processing images
-training_data = pickle.load(open(training_data_pickle_path, "rb"))
+#training_data = pickle.load(open(training_data_pickle_path, "rb"))
 
 # Process the test images and load the test data
 print('Loading testing data')
-#test_data = make_dataset(settings.test_data_folder, feature_path, data_set_list)
-#pickle.dump(test_data, open(test_data_pickle_path, "wb"))
+test_data = make_dataset(settings.test_data_folder, feature_path, data_set_list)
+pickle.dump(test_data, open(test_data_pickle_path, "wb"))
 # Comment the above two lines and uncomment the following line to avoid re-processing images
-test_data = pickle.load(open(test_data_pickle_path, "rb"))
+#test_data = pickle.load(open(test_data_pickle_path, "rb"))
 
 ###########################################################################################
 # Build Classifier
@@ -53,10 +53,10 @@ multi_svm_classifier_pickle_path = join(settings.dumps_folder, 'multi_svm_classi
 # Build a classifier using the training_data
 print('Building the classifier using epochs={0} and svm_params={1}'.format(
     epochs, svm_params), end=' ')
-#multi_svm_classifier = MultiClassClassifier(training_data, epochs, svm_params)
-#pickle.dump(multi_svm_classifier, open(multi_svm_classifier_pickle_path, "wb"))
+multi_svm_classifier = MultiClassClassifier(training_data, epochs, svm_params)
+pickle.dump(multi_svm_classifier, open(multi_svm_classifier_pickle_path, "wb"))
 # Comment the above two lines and uncomment the following line to avoid re-building classifier
-multi_svm_classifier = pickle.load(open(multi_svm_classifier_pickle_path, "rb"))
+#multi_svm_classifier = pickle.load(open(multi_svm_classifier_pickle_path, "rb"))
 
 
 ##########################################################################################
@@ -69,5 +69,5 @@ for class_id, results in accuracy_results.items():
     results['accuracy_percentage'] = format((results['right'] / results['total'])*100,'.2f')
     sum = sum + float(format((results['right'] / results['total'])*100,'.2f'))
     print('class id: {}'.format(class_id), results)
-avg = sum/42
+avg = sum/11
 print(avg)
